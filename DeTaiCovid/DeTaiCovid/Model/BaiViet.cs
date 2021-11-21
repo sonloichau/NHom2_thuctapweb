bf26b19767cdc26de5554f1c8c7396cfb2b47071
sonloichau <83951302+sonloichau@users.noreply.github.com>
2021-11-21 23:32:04 +0700

    sonUpdate

---
+++ DeTaiCovid/DeTaiCovid/Model/BaiViet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DeTaiCovid.Model
+{
+    public partial class BaiViet
+    {
+        public int BaiVietId { get; set; }
+        public string TieuDe { get; set; }
+        public string MoTa { get; set; }
+        public string NoiDung { get; set; }
+        public int? ChuDeid { get; set; }
+        public string Anh { get; set; }
+        public DateTime? NgayTao { get; set; }
+
+        public virtual ChuDe ChuDe { get; set; }
+    }
+}
